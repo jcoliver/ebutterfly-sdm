34a45ec21713f5661343138d9cc8402d75e7e7fd
jcoliver <jcoliver@email.arizona.edu>
2017-10-16 10:12:23 -0700

    Add preprocessing script of GBIF records

---
+++ scripts/gbif-butterflies.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Extract butterfly records from GBIF Darwin Core Archive verbatim.txt file from:
+# https://www.google.com/url?q=https%3A%2F%2Fwww.gbif.org%2Fdataset%2F50c9509d-22c7-4a22-a47d-8c48425ef4a7&sa=D&sntz=1&usg=AFQjCNEzY1KC-xcJO1vgk6fhrSW-1_FoCA
+# The verbatim.txt file is tab-delimited text of some 2e6 records from
+# iNaturalist. This script is pulling out a subset of the records (those
+# with lines matching one of the butterfly family names) for subsequent
+# processing to extract taxon_id values
+
+INFILE="../data/gbif/verbatim.txt"
+OUTFILE="../data/gbif/verbatim-butterflies.txt"
+
+# Get header row for our output file
+head -n1 $INFILE > $OUTFILE
+
+# Iterate over all families and copy those lines to output file
+FAMILIES=("Hesperiidae" "Papilionidae" "Pieridae" "Nymphalidae" "Lycaenidae" "Riodinidae")
+for FAMILY in "${FAMILIES[@]}";
+do
+  echo "Processing $FAMILY"
+  # grep $FAMILY $INFILE | wc -l
+  grep $FAMILY $INFILE >> $OUTFILE
+done
+
