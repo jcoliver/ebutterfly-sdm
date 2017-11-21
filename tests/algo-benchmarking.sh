2f94920eb0a6a9fc869ad0b1736e64d1eebfb370
jcoliver <jcoliver@email.arizona.edu>
2017-11-20 18:20:10 -0700

    Add test for run-sdm-algo benchmarking

---
+++ tests/algo-benchmarking.sh
@@ -0,0 +1,5 @@
+time Rscript --vanilla scripts/run-sdm-algo.R data/inaturalist/59125-iNaturalist.txt 59125 output/ CTA 10 0.7
+
+time Rscript --vanilla scripts/run-sdm-algo.R data/inaturalist/59125-iNaturalist.txt 59125 output/ GLM 10 0.7
+
+time Rscript --vanilla scripts/run-sdm-algo.R data/inaturalist/59125-iNaturalist.txt 59125 output/ RF 10 0.7
